8e5d48eca57ac8d42ae0925a0eb91bdf2f4c9973
ravion223 <tablettaoson2016@gmail.com>
2024-08-12 12:58:30 +0300

    minor changes + chat with ajax

--- chats/urls.py
+++ chats/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('chat/<str:room_name>/', views.chat_room, name='chat-room'),
     path('start_chat/<int:friend_id>/', views.start_chat, name='start-chat'),
     path('update-message/<int:message_id>/', views.update_message, name='update-message'),
-    path('delete-message/<int:message_id>/', views.delete_message, name='delete-message')
+    path('delete-message/<int:message_id>/', views.delete_message, name='delete-message'),
+    path('chat/<str:room_name>/check_new_messages/', views.check_new_messages, name='check-new-messages')
 ]
 
 app_name = 'chats'
